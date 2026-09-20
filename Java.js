eb101c791a76df6924ddad10d0a8795b5bf1b5e3
Anas Ghawanmeh <aghawanmeh857@gmail.com>
2026-09-20 19:26:18 +0300

    Initial commit

---
+++ Java.js
@@ -0,0 +1,52 @@
+
+let name;
+let age;
+let gender;
+let order_Status;
+let order;
+
+name = prompt("Enter your name");
+age = Number(prompt("Enter your age"));
+gender = prompt("Enter your gender");
+
+// Welcome Message
+if (gender == "male") {
+    document.write("Welcome Mr. " + name + "<br>");
+}
+else if (gender == "female") {
+    document.write("Welcome Ms. " + name + "<br>");
+}
+else {
+    document.write("Welcome " + name + "<br>");
+}
+
+
+if (age < 16) {
+
+    alert("You are not eligible to place an order");
+
+}
+else {
+
+    
+    order = prompt("Choose an Order: Burger, Shawarma, Zinger");
+
+    
+    if (order == "Burger" || order == "Shawarma" || order == "Zinger") {
+
+        alert("Your order is being prepared");
+        document.write("Your order: " + order + "<br>");
+
+    }
+    else {
+
+        alert("Invalid order. Please try again");
+
+    }
+}
+
+
+document.write("Customer Name: " + name + "<br>");
+document.write("Age: " + age + "<br>");
+document.write("Your Order: " + order + "<br>");
+
